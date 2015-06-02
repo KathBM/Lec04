18d30850b7b806e175aa804df778d6c3a58f9d24
KathBM <kathbmcr@hotmail.com>
2015-06-01 21:00:40 -0600

    Leccion4 Casi terminado

---
+++ Leccion04/BaseDeDatos/Nodo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseDeDatos
+{
+    private class Nodo
+    {
+        private int agregar;
+        private Nodo numeros;
+
+        private Nodo(int agregar)
+        {
+            Agregar = agregar;
+            Numeros = null;
+
+        }
+        private Nodo Numeros
+        {
+            get {return numeros; }
+            set {numeros = value; }
+        
+         private int Agregar
+        {
+            get {return agregar; }
+            set {agregar= value; }
+         }
+        }
+    }
+}
---
+++ Leccion04/Consola/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //try
+           // {
+             //   StreamReader streamReader = new StreamReader("Calculadora");
+              //  Console.WriteLine(streamReader.ReadToEnd());
+                //streamReader.Close();
+            //}
+            //catch(Exception ex)
+            //{
+              //  Console.WriteLine(ex.Message);
+                //Console.WriteLine();
+                //Console.WriteLine();
+                //Console.WriteLine(ex.StackTrace);
+            
+            List<int> entrada = new List<int>();
+            entrada.Add(4);
+            entrada.Add(6);
+            entrada.Add(20);
+            entrada.Add(1);
+            entrada.Add(11);
+            entrada.Add(25);
+
+            Calculadora laCalculadora = new Calculadora();
+
+            List<int> salida =  laCalculadora.CalcularFibonacciDeLista(entrada);
+
+            foreach (int i in salida)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
